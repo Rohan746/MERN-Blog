1e6bfbc48d251bd585fd0e3b12478b881a7e1979
Rohan746 <sainirohan2002@gmail.com>
2024-02-20 19:13:43 +0530

    complete profilepage UI

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -17,11 +17,15 @@ export default function DashSidebar() {
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Link to="/dashboard?tab=profile">
-            <Sidebar.Item active icon={HiUser} label={"User"} labelColor="dark">
-              Profile
-            </Sidebar.Item>
-          </Link>
+          <Sidebar.Item
+            to="/dashboard?tab=profile"
+            active={tab === "profile"}
+            icon={HiUser}
+            label={"User"}
+            labelColor="dark"
+          >
+            Profile
+          </Sidebar.Item>
           <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
             Sign Out
           </Sidebar.Item>
