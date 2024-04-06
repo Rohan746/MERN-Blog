d6e02dbc3c381312f4ae7a3fde6910f93a37a7c5
Rohan746 <sainirohan2002@gmail.com>
2024-04-06 19:29:44 +0530

    updating dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
@@ -11,5 +11,8 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  // more configurations...
 })
